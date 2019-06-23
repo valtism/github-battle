4adea0c1a26b8c008236a50a4685b7cf30ce83bb
Dan Wood <daniel4wood@icloud.com>
2019-06-23 15:38:21 +1000

    Handle player state in Battle

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -84,14 +84,45 @@ PlayerInput.PropTypes = {
 };
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    });
+  }
   render() {
+    const { playerOne, playerTwo } = this.state;
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput
-          label="Label!"
-          onSubmit={value => console.log("Value!", value)}
-        />
+
+        <div className="players-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-around">
+            {playerOne === null && (
+              <PlayerInput
+                label="Player One"
+                onSubmit={value => this.handleSubmit("playerOne", value)}
+              />
+            )}
+
+            {playerTwo === null && (
+              <PlayerInput
+                label="Player Two"
+                onSubmit={value => this.handleSubmit("playerTwo", value)}
+              />
+            )}
+          </div>
+        </div>
       </React.Fragment>
     );
   }
