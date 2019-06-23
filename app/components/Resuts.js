545b307bd040dc04c3c3a2dcf5ed39755237bfd4
Dan Wood <daniel4wood@icloud.com>
2019-06-23 22:17:50 +1000

    Create loading component

--- app/components/Resuts.js
+++ app/components/Resuts.js
@@ -10,6 +10,7 @@ import {
 } from "react-icons/fa";
 import Card from "./Card";
 import PropTypes from "prop-types";
+import Loading from "./Loading";
 
 function ProfileList({ profile }) {
   return (
@@ -81,7 +82,7 @@ export default class Results extends React.Component {
     const { winner, loser, error, loading } = this.state;
 
     if (loading === true) {
-      return <p>Loading</p>;
+      return <Loading text="Battling"/>;
     }
 
     if (error) {
