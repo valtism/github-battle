6b9a62d52b9407e0df6f7843e5686395a6888b4d
Dan Wood <daniel4wood@icloud.com>
2019-06-23 18:11:29 +1000

    Fetch profile data and calculate scores.

--- app/components/Resuts.js
+++ app/components/Resuts.js
@@ -1,12 +1,18 @@
 import React from "react";
+import { battle } from "../utils/api";
 
 export default class Results extends React.Component {
-    render() {
-        return (
-            <div>
-                Results
-                <pre>{JSON.stringify(this.props, null, 2)}</pre>
-            </div>
-        )
-    }
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+
+    battle([playerOne, playerTwo]).then(players => console.log(players));
+  }
+  render() {
+    return (
+      <div>
+        Results
+        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+      </div>
+    );
+  }
 }
